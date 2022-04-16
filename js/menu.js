03fc730e17cc7c7defa0ead7d5785995411bde4c
Isai Jesus <isaijesus02@gmail.com>
2022-04-16 00:27:42 -0500

    Fix Menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,18 @@
 var menu = document.getElementById('menu');
+var link1 = document.getElementById('link1');
+var link2 = document.getElementById('link2');
+var link3 = document.getElementById('link3');
+var link4 = document.getElementById('link4');
+var link5 = document.getElementById('link5');
+var menuResume = document.querySelector('.menu-resume');
 
 menu.addEventListener('click', handleToggle);
+link1.addEventListener('click', handleToggle);
+link2.addEventListener('click', handleToggle);
+link3.addEventListener('click', handleToggle);
+link4.addEventListener('click', handleToggle);
+link5.addEventListener('click', handleToggle);
+menuResume.addEventListener('click', handleToggle);
 
 function handleToggle() {
   document.querySelector(".menu-container").classList.toggle("menu-off");
