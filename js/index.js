ddc067f39aa6f958350fff9f2190dfd1193ab441
Isai Jesus <isaijesus02@gmail.com>
2022-09-25 00:14:22 -0500

    Fixing animation

--- js/index.js
+++ js/index.js
@@ -5,27 +5,27 @@ window.sr = ScrollReveal();
 sr.reveal('#iam-h1', {
   duration: 1400,
   origin: 'top',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#iam-h2', {
   duration: 1200,
   origin: 'top',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#iam-p', {
   duration: 1000,
   origin: 'top',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.resume-mobile', {
   duration: 800,
   origin: 'top',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.iam-img', {
   duration: 1400,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 
 /*ABOUT*/
@@ -33,12 +33,12 @@ sr.reveal('.iam-img', {
 sr.reveal('#about-header', {
   duration: 1000,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#about-p', {
   duration: 1200,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 
 /*EXPERIENCE*/
@@ -46,27 +46,27 @@ sr.reveal('#about-p', {
 sr.reveal('#experience-header', {
   duration: 1000,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#experience-li1', {
   duration: 1000,
   origin: 'left',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#experience-li2', {
   duration: 1200,
   origin: 'left',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#experience-li3', {
   duration: 1400,
   origin: 'left',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#experience-li3', {
   duration: 1600,
   origin: 'left',
-  distance: '50px'
+  distance: '30px'
 });
 
 /*WORK*/
@@ -74,42 +74,42 @@ sr.reveal('#experience-li3', {
 sr.reveal('#work-header', {
   duration: 1000,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#card1', {
   duration: 1200,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#card2', {
   duration: 1400,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#card3', {
   duration: 1600,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#card4', {
   duration: 1200,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#card5', {
   duration: 1400,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#card6', {
   duration: 1600,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.more', {
   duration: 1000,
   origin: 'left',
-  distance: '50px'
+  distance: '30px'
 });
 
 /*SKILLS*/
@@ -117,12 +117,12 @@ sr.reveal('.more', {
 sr.reveal('#skills-header', {
   duration: 1000,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.skills-box', {
   duration: 1200,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 
 /*CONTACT*/
@@ -130,35 +130,35 @@ sr.reveal('.skills-box', {
 sr.reveal('#contact-header', {
   duration: 800,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('#contact-h5', {
   duration: 1000,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.social-box', {
   duration: 1200,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.name', {
   duration: 1400,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.email', {
   duration: 1400,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.message', {
   duration: 1400,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
 sr.reveal('.send', {
   duration: 1600,
   origin: 'bottom',
-  distance: '50px'
+  distance: '30px'
 });
