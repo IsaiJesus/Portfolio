f5b6c9c57b907cb2e85ad1c3ff77f7513998cf65
Isai Jesus <isaijesus02@gmail.com>
2025-01-19 00:28:58 -0600

    Update CV, experience and projects

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,11 @@
-var menu = document.getElementById('menu');
-var link1 = document.getElementById('link1');
-var link2 = document.getElementById('link2');
-var link3 = document.getElementById('link3');
-var link4 = document.getElementById('link4');
-var menuResume = document.querySelector('.menu-resume');
+var barMenu = document.getElementById('bar-menu');
+var linkButtons = document.querySelectorAll('.link-button');
 
-menu.addEventListener('click', handleToggle);
-link1.addEventListener('click', handleToggle);
-link2.addEventListener('click', handleToggle);
-link3.addEventListener('click', handleToggle);
-link4.addEventListener('click', handleToggle);
-menuResume.addEventListener('click', handleToggle);
+barMenu.addEventListener('click', handleToggle);
+linkButtons.forEach(function(button) {
+  button.addEventListener('click', handleToggle);
+});
 
 function handleToggle() {
-  document.querySelector(".menu-container").classList.toggle("menu-on");
+  document.querySelector(".nav-mobile-box").classList.toggle("nav-mobile-on");
 }
